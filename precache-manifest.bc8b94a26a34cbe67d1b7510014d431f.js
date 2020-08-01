57077b86c7b9e5a511d4513446c4d39365529972
Unnati Verma <unavee.24@gmail.com>
2020-08-01 17:59:42 +0530

    Updates

---
+++ precache-manifest.bc8b94a26a34cbe67d1b7510014d431f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "576deb5e13d434e4925827530f917dbb",
+    "url": "/hacker-news-reader/index.html"
+  },
+  {
+    "revision": "c2dfcc853ae9973ed53d",
+    "url": "/hacker-news-reader/static/css/main.9234e83d.chunk.css"
+  },
+  {
+    "revision": "08b074bd05edd877067e",
+    "url": "/hacker-news-reader/static/js/2.fc230348.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/hacker-news-reader/static/js/2.fc230348.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2dfcc853ae9973ed53d",
+    "url": "/hacker-news-reader/static/js/main.8accb13b.chunk.js"
+  },
+  {
+    "revision": "80de6268789ebd6493f8",
+    "url": "/hacker-news-reader/static/js/runtime-main.6195e27a.js"
+  }
+]);
